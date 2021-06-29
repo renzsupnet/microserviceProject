125fc32e659933acd1fd37cbb177fa0d5e4aebfd
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2021-06-28 22:47:02 -0700

    Several major changes:
    1) Deployment and service.yaml files for each microservice
    2) environment variables
    3) AWS eks cluster integrated
    4) reverse-proxy integrated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a86f8b5499c914188936a4e3b19d45e4-162964564.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
